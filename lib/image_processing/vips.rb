7d04763ff4e92b1b19060fadba89058640681bde
GustavoCaso <gustavocaso@gmail.com>
2017-10-01 14:20:33 +0200

    Move _copy_to_tempfile to test_helper

--- lib/image_processing/vips.rb
+++ lib/image_processing/vips.rb
@@ -189,16 +189,6 @@ module ImageProcessing
       destination
     end
 
-    # Creates a copy of the file and stores it into a Tempfile. Works for any
-    # IO object that responds to `#read(length = nil, outbuf = nil)`.
-    def _copy_to_tempfile(file)
-      extension = File.extname(file.path) if file.respond_to?(:path)
-      tempfile = Tempfile.new(["vips", extension.to_s], binmode: true)
-      IO.copy_stream(file, tempfile.path)
-      file.rewind
-      tempfile
-    end
-
     def _tempfile(extension)
       Tempfile.new(["image_processing-vips", extension.to_s], binmode: true)
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,13 @@ require "phashion" unless RUBY_ENGINE == "jruby"
 def fixture_image(name)
   File.open("test/fixtures/#{name}")
 end
+
+# Creates a copy of the file and stores it into a Tempfile. Works for any
+# IO object that responds to `#read(length = nil, outbuf = nil)`.
+def _copy_to_tempfile(file)
+  extension = File.extname(file.path) if file.respond_to?(:path)
+  tempfile = Tempfile.new(["test", extension.to_s], binmode: true)
+  IO.copy_stream(file, tempfile.path)
+  file.rewind
+  tempfile
+end
