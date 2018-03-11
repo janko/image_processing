1b1f85c9428a4230fe1d834fc1c230edc7de6fd0
Janko Marohnić <janko.marohnic@gmail.com>
2018-03-12 00:11:22 +0100

    Correct some RDocs

--- lib/image_processing/vips.rb
+++ lib/image_processing/vips.rb
@@ -19,7 +19,7 @@ module ImageProcessing
     # since we want the user to have a destructive and a non destructive method
     # using the destructive method will cause the original file to be deleted
     #
-    # @param [#read] file              the image to convert
+    # @param [#path, #read] file       the image to convert
     # @param [String] format           the format to convert to
     # @yield [Vips::Image]
     # @return [Tempfile]
@@ -29,7 +29,7 @@ module ImageProcessing
 
     # Adjusts the image so that its orientation is suitable for viewing.
     #
-    # @param [#read] file              the image to convert
+    # @param [#path, #read] file       the image to convert
     # @yield [Vips::Image]
     # @return [Tempfile]
     # @see http://jcupitt.github.io/libvips/API/current/libvips-conversion.html#vips-autorot
@@ -46,10 +46,10 @@ module ImageProcessing
     # narrower than specified in either dimension but will not be larger than
     # the specified values.
     #
-    # @param [#read] file              the image to convert
+    # @param [#path, #read] file       the image to convert
     # @param [#to_s] width             the maximum width
     # @param [#to_s] height            the maximum height
-    # @param [options]                 options for Vips::Image#thumbnail_image
+    # @param [Hash] options            options for Vips::Image#thumbnail_image
     # @yield [Vips::Image]
     # @return [Tempfile]
     # @see http://www.rubydoc.info/gems/ruby-vips/Vips/Image#thumbnail_image-instance_method
@@ -65,10 +65,10 @@ module ImageProcessing
     # specified in the smaller dimension but will not be larger than the
     # specified values.
     #
-    # @param [#read] file              the image to convert
+    # @param [#path, #read] file       the image to convert
     # @param [#to_s] width             the width to fit into
     # @param [#to_s] height            the height to fit into
-    # @param [options]                 options for Vips::Image#thumbnail_image
+    # @param [Hash] options            options for Vips::Image#thumbnail_image
     # @yield [Vips::Image]
     # @return [Tempfile]
     # @see http://www.rubydoc.info/gems/ruby-vips/Vips/Image#thumbnail_image-instance_method
@@ -88,10 +88,10 @@ module ImageProcessing
     # By default, the center part of the image is kept, and the remainder
     # cropped off, but this can be changed via the `gravity` option.
     #
-    # @param [#read] file              the image to convert
+    # @param [#path, #read] file       the image to convert
     # @param [#to_s] width             the width to fill out
     # @param [#to_s] height            the height to fill out
-    # @param [options]                 options for Vips::Image#thumbnail_image
+    # @param [Hash] options            options for Vips::Image#thumbnail_image
     # @yield [Vips::Image]
     # @return [Tempfile]
     # @see http://www.rubydoc.info/gems/ruby-vips/Vips/Image#thumbnail_image-instance_method
@@ -114,12 +114,12 @@ module ImageProcessing
     # By default, the image will be placed in the center but this can be
     # changed via the `gravity` option.
     #
-    # @param [#read] file               the image to convert
+    # @param [#path, #read] file        the image to convert
     # @param [#to_s] width              the width to fill out
     # @param [#to_s] height             the height to fill out
     # @param [String] background        the color to use as a background
     # @param [String] gravity           which part of the image to focus on
-    # @param [options]                  options for Vips::Image#thumbnail_image
+    # @param [Hash] options             options for Vips::Image#thumbnail_image
     # @yield [Vips::Image]
     # @return [Tempfile]
     # @see http://www.rubydoc.info/gems/ruby-vips/Vips/Image#thumbnail_image-instance_method
@@ -137,7 +137,7 @@ module ImageProcessing
 
     # Crops the image to be the defined area.
     #
-    # @param [#read] file               the image to convert
+    # @param [#path, #read] file        the image to convert
     # @param [#to_s] width              the width of the cropped image
     # @param [#to_s] height             the height of the cropped image
     # @param [#to_s] x_offset           the x coordinate where to start cropping
