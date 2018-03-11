1c04af84c903993416b5890da09770ebd781916b
Janko Marohnić <janko.marohnic@gmail.com>
2018-03-12 00:23:59 +0100

    List gravities in a more compact way

--- lib/image_processing/vips/gravity.rb
+++ lib/image_processing/vips/gravity.rb
@@ -9,24 +9,15 @@ module ImageProcessing
         top = image.width - width
         left = image.height - height
         values = case gravity
-                  when 'Center'
-                    [(top / 2), (left / 2)]
-                  when 'North'
-                    [(top / 2), 0]
-                  when 'East'
-                    [top, (left / 2)]
-                  when 'South'
-                    [(top / 2), left]
-                  when 'West'
-                    [0, (left / 2)]
-                  when 'NorthEast'
-                    [top, 0]
-                  when 'SouthEast'
-                    [top,left]
-                  when 'SouthWest'
-                    [0, left]
-                  when 'NorthWest'
-                    [0, 0]
+                  when "Center"    then [(top / 2), (left / 2)]
+                  when "North"     then [(top / 2), 0]
+                  when "East"      then [top, (left / 2)]
+                  when "South"     then [(top / 2), left]
+                  when "West"      then [0, (left / 2)]
+                  when "NorthEast" then [top, 0]
+                  when "SouthEast" then [top,left]
+                  when "SouthWest" then [0, left]
+                  when "NorthWest" then [0, 0]
                   else
                     raise InvalidGravityValue
                   end
