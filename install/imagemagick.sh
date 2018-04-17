c60d933fb2385fb852a8845d6c6406c56104125c
Janko Marohnić <janko.marohnic@gmail.com>
2018-04-17 11:17:22 +0200

    Cache libvips and ImageMagick builds on Travis CI

--- install/imagemagick.sh
+++ install/imagemagick.sh
@@ -1,7 +1,23 @@
-wget http://www.imagemagick.org/download/ImageMagick.tar.gz
-tar -xvf ImageMagick.tar.gz
+#!/bin/bash
+
+set -e
+
+export MAGICK_HOME=$HOME/ImageMagick
+export PATH=$MAGICK_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$MAGICK_HOME/lib:$LD_LIBRARY_PATH
+
+magick_site=http://www.imagemagick.org/download
+
+# return early if we've already built ImageMagick
+if [ -d "$MAGICK_HOME/bin" ]; then
+  echo "Using cached directory"
+  exit 0
+fi
+
+rm -rf $MAGICK_HOME
+
+wget $magick_site/ImageMagick.tar.gz
+tar zvxf ImageMagick.tar.gz
 cd ImageMagick-7.*
-./configure
-make
-sudo make install
-sudo ldconfig /usr/local/lib
+./configure --prefix=$MAGICK_HOME $*
+make & make install
