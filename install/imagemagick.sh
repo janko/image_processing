34420818aa263e02c54f3af0ece424be88589d4c
Janko Marohnić <janko.marohnic@gmail.com>
2018-04-18 16:57:49 +0200

    Revert "Cache libvips and ImageMagick builds on Travis CI"
    This reverts commit c60d933fb2385fb852a8845d6c6406c56104125c.
    
    This is still a work-in-progress that was accidentally committed.

--- install/imagemagick.sh
+++ install/imagemagick.sh
@@ -1,23 +1,7 @@
-#!/bin/bash
-
-set -e
-
-export MAGICK_HOME=$HOME/ImageMagick
-export PATH=$MAGICK_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$MAGICK_HOME/lib:$LD_LIBRARY_PATH
-
-magick_site=http://www.imagemagick.org/download
-
-# return early if we've already built ImageMagick
-if [ -d "$MAGICK_HOME/bin" ]; then
-  echo "Using cached directory"
-  exit 0
-fi
-
-rm -rf $MAGICK_HOME
-
-wget $magick_site/ImageMagick.tar.gz
-tar zvxf ImageMagick.tar.gz
+wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+tar -xvf ImageMagick.tar.gz
 cd ImageMagick-7.*
-./configure --prefix=$MAGICK_HOME $*
-make & make install
+./configure
+make
+sudo make install
+sudo ldconfig /usr/local/lib
--- install/libvips.sh
+++ install/libvips.sh
@@ -1,33 +1,8 @@
-#!/bin/bash
-
-set -e
-
-export VIPS_HOME=$HOME/libvips
-export PATH=$VIPS_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$VIPS_HOME/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$VIPS_HOME/lib/pkgconfig:$PKG_CONFIG_PATH
-export GI_TYPELIB_PATH=$VIPS_HOME/lib/girepository-1.0/
-
 vips_site=https://github.com/jcupitt/libvips/releases/download
-vips_version_major=8
-vips_version_minor=6
-vips_version_patch=3
-version=$vips_version_major.$vips_version_minor.$vips_version_patch
+version=8.6.3
 
-# return early if we've already built this version of libvips
-if [ -d "$VIPS_HOME/bin" ]; then
-  installed_version=$(vips --version)
-  escaped_version="$vips_version_major\.$vips_version_minor\.$vips_version_patch"
-  if [[ "$installed_version" =~ ^vips-$escaped_version ]]; then
-    echo "Using cached directory"
-    exit 0
-  fi
-fi
-
-rm -rf $VIPS_HOME
-
-wget $vips_site/v$version/vips-$version.tar.gz
-tar zvxf vips-$version.tar.gz
-cd vips-$version
-./configure --prefix=$VIPS_HOME $*
-make & make install
+sudo apt-get install -y gobject-introspection libgirepository1.0-dev libglib2.0-dev libpoppler-glib-dev
+curl -OL $vips_site/v$version/vips-$version.tar.gz
+tar zvxf vips-$version.tar.gz && cd vips-$version && ./configure && sudo make && sudo make install
+export GI_TYPELIB_PATH=/usr/local/lib/girepository-1.0/
+sudo ldconfig
