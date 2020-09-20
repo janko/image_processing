d49c59fb558dace8d6c5a0dd4401475405db1636
Janko Marohnić <janko.marohnic@gmail.com>
2020-09-20 21:37:39 +0200

    Try testing on libvips 8.10.1
    Hopefully that fixes the Travis CI failures.

--- install/libvips.sh
+++ install/libvips.sh
@@ -1,5 +1,5 @@
 vips_site=https://github.com/libvips/libvips/releases/download
-version=8.9.0
+version=8.10.1
 
 sudo apt-get install -y gobject-introspection libgirepository1.0-dev libglib2.0-dev libpoppler-glib-dev
 curl -OL $vips_site/v$version/vips-$version.tar.gz
