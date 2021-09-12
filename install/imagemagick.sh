9c4da90eafedea28273f9fcafb45304baeda7e41
Janko Marohnić <janko.marohnic@gmail.com>
2021-09-12 21:43:31 +0200

    Remove install scripts
    On Travis CI we were installing from source, but on GitHub CI we're
    installing the packages via APT.

--- install/imagemagick.sh
+++
@@ -1,7 +0,0 @@
-wget http://www.imagemagick.org/download/ImageMagick.tar.gz
-tar -xvf ImageMagick.tar.gz
-cd ImageMagick-7.*
-./configure
-make
-sudo make install
-sudo ldconfig /usr/local/lib
--- install/libvips.sh
+++
@@ -1,8 +0,0 @@
-vips_site=https://github.com/libvips/libvips/releases/download
-version=8.10.1
-
-sudo apt-get install -y gobject-introspection libgirepository1.0-dev libglib2.0-dev libpoppler-glib-dev
-curl -OL $vips_site/v$version/vips-$version.tar.gz
-tar zvxf vips-$version.tar.gz && cd vips-$version && ./configure && sudo make && sudo make install
-export GI_TYPELIB_PATH=/usr/local/lib/girepository-1.0/
-sudo ldconfig
